65983fe6853391b116b18f740c280d2c2a33a856
José Manuel Ciges Regueiro <jmanuel@ciges.net>
2020-06-09 09:37:04 +0200

    Updated code after 'Programming Concepts: Objetc-Oriented Programming Basics' lesson

---
+++ creating_classes.py
@@ -0,0 +1,20 @@
+class Car:
+	"""
+	Docstring describing the class
+	"""
+
+	def __init__(self, engine, tires):
+		"""
+		Docstring describing the method
+		"""
+		self.engine = engine
+		self.tires = tires
+		
+	def description(self):
+		print(f"A car with an {self.engine} engine, and {self.tires}")
+
+	def wheel_circumference(self):
+		if len(self.tires) > 0:
+			return self.tires[0].circumference()
+		else:
+			return 0
